1f7511d79a4ed9a94c8a2b4394dd6ce031428ec9
siennawheeler <sienna_wheeler1@my.vcccd.edu>
2024-06-27 06:42:47 +0000

    :(((

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def main():
     n = 2
     m = 0
     while begin <= end: #dividing, if get whole number, move on to next number // if get decimal, that is prime (?) // go until dividing by end
-        for n in range(n >= 2 and n <= int((begin ** (1 / 2)) + 1)):
+        for n in range(2, int((begin ** (1 / 2)) + 1)):
             m = begin % n
             if m != 0:
                 plist.append(begin) # if x is prime, add it to the list    
