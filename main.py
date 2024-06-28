6d4dfc71ebab569a660c1bc5a11a5007432b7662
siennawheeler <sienna_wheeler1@my.vcccd.edu>
2024-06-28 02:30:01 +0000

    asdf

--- main.py
+++ main.py
@@ -5,23 +5,31 @@ def main():
     ##################################################
     
     begin = int(input('begin: '))
-    if begin <= 1:
+    while begin <= 1:
         begin = int(input('value must be greater than 1: '))
     end = int(input('end: '))
-    if begin > end:
+    while begin >= end:
         end = int(input('please enter a value that is greater than the first: '))
 
-    n = 2
-    m = 0
+    if begin == 2:
+        plist.append(begin)
+        begin = begin + 1
     while begin <= end: #dividing, if get whole number, move on to next number // if get decimal, that is prime (?) // go until dividing by end
-        for n in range(2, int((begin ** (1 / 2)) + 1)):
-            m = begin % n
-            if m == 0:
-                begin = begin + 1
-            else:
-                plist.append(begin) # if x is prime, add it to the list    
-                begin = begin + 1
-            
+        # check if even
+        if (begin % 2) == 0:
+            begin = begin + 1
+            continue
+        # check odds to halfway point
+        n = 3
+        half = (begin // 2)
+        while n < half:
+            if (begin % n) == 0:
+                break
+            n = n + 2
+        if (n >= half - 1):
+            plist.append(begin)
+        begin = begin + 1
+        
             
     print (plist)
     return plist
